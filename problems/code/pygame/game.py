bb0533d1d1186aab0618cb8462496b97d714aa65
Scott Little <scott.alan.little@gmail.com>
2016-01-19 16:11:42 -0600

    2nd commit

--- problems/code/pygame/game.py
+++ problems/code/pygame/game.py
@@ -2,8 +2,11 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((640, 480))
+#DISPLAYSURF = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Hello, Pygame!")
+
+BLUE = (0, 0, 255)
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 
 # Main game loop
 while True:
@@ -12,4 +15,5 @@ while True:
             pygame.quit()
             sys.exit()
 
+    pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
     pygame.display.update()
